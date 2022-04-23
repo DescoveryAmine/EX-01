7db608c50dda901a214ec8a21187a8b4b90755df
Djebbi Amine <aminedjebbie@gmail.com>
2022-04-24 00:33:51 +0200

    Init-Project

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+
+
+const app = express();
+
+// A-Body parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// B-DB ConfIg
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport CoNfig
+require('./config/passport')(passport);
+
+// UsE Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));

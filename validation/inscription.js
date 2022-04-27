25787fc5869fde93abb92c5d2efb09a23d61c7d8
Djebbi Amine <aminedjebbie@gmail.com>
2022-04-27 06:25:24 +0200

    ok

--- validation/inscription.js
+++ validation/inscription.js
@@ -5,14 +5,9 @@ module.exports = function validateSubmitInscription(data) {
   let errors = {};
 
   data.userid = !isEmpty(data.userid) ? data.userid : '';
-  data.password = !isEmpty(data.password) ? data.password : '';
 
   if (Validator.isEmpty(data.userid)) {
     errors.userid = 'userId field is required';
-  }
-
-  if (Validator.isEmpty(data.password)) {
-    errors.password = 'Password field is required';
   }
 
   return {
